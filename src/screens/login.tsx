00deb5679e7d209e3c0bb5389e5eb072d021842e
Capitão Sousa <raimundoalvessousa19@gmail.com>
2025-11-26 20:26:49 -0300

    refactor :alteração da estrutura identificação tailwind

---
+++ src/screens/login.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import { red } from "react-native-reanimated/lib/typescript/Colors";
+
+export function LoginScreen() {
+  return(
+    <View className="flex-1 items-center justify-center bg-orange-300">
+        <Text className="text-6xl">Tela de login</Text>
+    </View>
+
+  )
+}
