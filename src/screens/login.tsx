f7137ecfc042c640dea1ca8c7b4ff8479ade550e
Capitão Sousa <raimundoalvessousa19@gmail.com>
2025-12-03 19:46:12 -0300

    style: add uma barra para dividir a caixa de texto esqueceu senha e cadastra-se

--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -6,6 +6,11 @@ import { Text, View } from 'react-native';
 export function LoginScreen() {
   const [login, setLogin] = useState('');
   const [senha, setSenha] = useState('');
+
+  function logar() {
+    console.log(`Login: ${login}, Senha: ${senha}`);
+  }
+
   return (
     <View className="flex-1 items-center justify-center bg-slate-200">
       <View className="w-full items-center gap-5 p-5">
@@ -13,20 +18,23 @@ export function LoginScreen() {
         <InputCustomizado
           placeholder={'Login'}
           onChangeText={(text) => {
-            setLogin(text);}}
+            setLogin(text);
+          }}
         />
         <InputCustomizado
           placeholder="senha"
           onChangeText={(text) => {
-            setSenha(text);}}
+            setSenha(text);
+          }}
           secureTextEntry={true}
         />
-        
-        <BotaoCustomizado
-          title="entrar"
-          onPress={() => console.log('Login:', login, 'Senha:', senha)}
-        />
-        <Text className="text-blue-400">Esqueceu a senha?</Text>
+
+        <BotaoCustomizado title="entrar" onPress={logar} />
+        <View className="flex-row gap-2">
+          <Text className="text-blue-400 text-xl">Esqueceu a senha?</Text>
+          <Text className="text-center">|</Text>
+          <Text className="text-blue-400 text-xl">Cadastra-se</Text>
+        </View>
       </View>
     </View>
   );
